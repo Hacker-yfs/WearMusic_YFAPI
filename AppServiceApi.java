395f83269783c8742e026a03218d3b42d575f24f
Mr.Hacker <86495643+Hacker-yfs@users.noreply.github.com>
2023-01-19 03:47:25 +0800

    Add files via upload

---
+++ AppServiceApi.java
@@ -0,0 +1,40 @@
+package cn.wearbbs.music.api;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.net.URLEncoder;
+
+import cn.wearbbs.music.application.MainApplication;
+import cn.wearbbs.music.util.SharedPreferencesUtil;
+import cn.jackuxl.util.NetWorkUtil;
+
+public class AppServiceApi {
+    public static JSONObject getLatestVersionInfo() {
+        NetWorkUtil.setDomain("");
+        JSONObject result = JSON.parseObject(NetWorkUtil.sendByGetUrl("https://wmusic.wearbbs.cn/AppService/update.json", null));
+        resetServer();
+        return result;
+    }
+
+    public static boolean feedback(String content) {
+        NetWorkUtil.setDomain("");
+        String token = "C6ZdL45yZgBjHCmk";
+        int code = 400;
+        try{
+            code = JSON.parseObject(NetWorkUtil.sendByGetUrl(String.format("https://wmusic.wearbbs.cn/AppService/feedback/?token=%s&content=%s", token, URLEncoder.encode(content, "utf-8")), null)).getInteger("code");
+        }
+        catch (Exception ignored){ }
+        resetServer();
+        return code == 200;
+    }
+
+    public static void resetServer(){
+        if(SharedPreferencesUtil.getString("server","vercel").equals("wearbbs")){
+            NetWorkUtil.setDomain("https://music-api.yunfengxm.com");
+        }
+        else{
+            NetWorkUtil.setDomain("https://music-api.yunfengxm.com");
+        }
+    }
+}
